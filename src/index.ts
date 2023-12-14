f6279beadb53dbdbb1eaa09956c041793ac8c859
adspacheco <andersonac7@outlook.com>
2023-12-14 15:36:36 -0300

    Add new routes and tests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
-
 const port = 3000;
 
-app.get("/api", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`);
 });
+
+export const myFunc = function (a: number, b: number): number {
+  return a + b;
+};
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+import { myFunc } from "..";
+
+it("should add two numbers", () => {
+  expect(myFunc(1, 3)).toBe(4);
+});
